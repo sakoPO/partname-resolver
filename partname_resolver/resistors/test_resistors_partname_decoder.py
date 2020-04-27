37a099518eebbb48e219401da412de2d081c86fb
sakoPO <you@example.com>
2020-04-27 06:46:47 +0200

    Fixed resistors test.

--- partname_resolver/resistors/test_resistors_partname_decoder.py
+++ partname_resolver/resistors/test_resistors_partname_decoder.py
@@ -1,8 +1,9 @@
 import unittest
-from decimal import Decimal
 from partname_resolver.resistors.tolerance import Tolerance
 from partname_resolver.resistors import resistors_partname_decoder
 from partname_resolver.resistors.resistor import Resistor
+from partname_resolver.resistors.resistance import Resistance
+from partname_resolver.resistors.power import Power
 
 
 class ResistorsPartnameResolverTestCase(unittest.TestCase):
@@ -12,9 +13,9 @@ class ResistorsPartnameResolverTestCase(unittest.TestCase):
         self.assertEqual('Royal Ohm', decoded.manufacturer)
         self.assertEqual('1206S4J0100T5E', decoded.partnumber)
         self.assertEqual('', decoded.series)
-        self.assertEqual(Decimal('10'), decoded.resistance)
+        self.assertEqual(Resistance('10'), decoded.resistance)
         self.assertEqual(Tolerance('5%'), decoded.tolerance)
-        self.assertEqual(Decimal('0.25'), decoded.power)
+        self.assertEqual(Power('0.25'), decoded.power)
         self.assertEqual('200V', decoded.max_working_voltage)
         self.assertEqual('1206', decoded.case)
         self.assertEqual('Lead (Pb) Free Plating Type/ RoHS compliant', decoded.note)
@@ -24,9 +25,9 @@ class ResistorsPartnameResolverTestCase(unittest.TestCase):
         self.assertEqual('Royal Ohm', decoded.manufacturer)
         self.assertEqual('4D02WGJ0240T', decoded.partnumber)
         self.assertEqual('', decoded.series)
-        self.assertEqual(Decimal('24'), decoded.resistance)
+        self.assertEqual(Resistance('24'), decoded.resistance)
         self.assertEqual(Tolerance('5%'), decoded.tolerance)
-        self.assertEqual(Decimal('0.0625'), decoded.power)
+        self.assertEqual(Power('0.0625'), decoded.power)
         self.assertEqual('50V', decoded.max_working_voltage)
         self.assertEqual('4D02', decoded.case)
         self.assertEqual('', decoded.note)
@@ -37,9 +38,9 @@ class ResistorsPartnameResolverTestCase(unittest.TestCase):
         self.assertEqual('Yaego', decoded.manufacturer)
         self.assertEqual('RC0402FR-07100RL', decoded.partnumber)
         self.assertEqual('RC', decoded.series)
-        self.assertEqual(Decimal('100'), decoded.resistance)
+        self.assertEqual(Resistance('100'), decoded.resistance)
         self.assertEqual(Tolerance('1%'), decoded.tolerance)
-        self.assertEqual(Decimal('0.0625'), decoded.power)
+        self.assertEqual(Power('0.0625'), decoded.power)
         self.assertEqual('50V', decoded.max_working_voltage)
         self.assertEqual('0402', decoded.case)
         self.assertEqual('General purpose chip resistors', decoded.note)
@@ -50,9 +51,9 @@ class ResistorsPartnameResolverTestCase(unittest.TestCase):
         self.assertEqual('Vishay', decoded.manufacturer)
         self.assertEqual('CRCW0805562RFKTA', decoded.partnumber)
         self.assertEqual('CRCW', decoded.series)
-        self.assertEqual(Decimal('562'), decoded.resistance)
+        self.assertEqual(Resistance('562'), decoded.resistance)
         self.assertEqual(Tolerance('1%'), decoded.tolerance)
-        self.assertEqual(Decimal('0.125'), decoded.power)
+        self.assertEqual(Power('0.125'), decoded.power)
         self.assertEqual('150V', decoded.max_working_voltage)
         self.assertEqual('0805', decoded.case)
         self.assertEqual('Lead (Pb)-Bearing Thick Film, Rectangular Chip Resistors', decoded.note)
@@ -63,9 +64,9 @@ class ResistorsPartnameResolverTestCase(unittest.TestCase):
         self.assertEqual('Bourns', decoded.manufacturer)
         self.assertEqual('CAT16-220J4LF', decoded.partnumber)
         self.assertEqual('CAT16', decoded.series)
-        self.assertEqual(Decimal('22'), decoded.resistance)
+        self.assertEqual(Resistance('22'), decoded.resistance)
         self.assertEqual(Tolerance('5%'), decoded.tolerance)
-        self.assertEqual(Decimal('0.25'), decoded.power)
+        self.assertEqual(Power('0.25'), decoded.power)
         self.assertEqual('50V', decoded.max_working_voltage)
         self.assertEqual('1206', decoded.case)
         self.assertEqual('Concave Terminations', decoded.note)
