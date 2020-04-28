312fad557dee60a20f87e4f2a787704d0e461262
sakoPO <you@example.com>
2020-04-28 04:49:08 +0200

    Updated nichicon_decoder added support for optional packing parameter.

--- partname_resolver/capacitors/nichicon_decoder.py
+++ partname_resolver/capacitors/nichicon_decoder.py
@@ -10,26 +10,26 @@ series = {'VZ': 'Wide Temperature Range',
           'CS': 'Miniature Sized, High Ripple Current, High Reliability',
           'CY': 'Miniature Sized, High Ripple Current, High Reliability'}
 
-voltage = {'0E': '2.5VDC',
-           '0G': '4VDC',
-           '0J': '6.3VDC',
-           '1A': '10VDC',
-           '1C': '16VDC',
-           '1E': '25VDC',
-           '1V': '35VDC',
-           '1H': '50VDC',
-           '1J': '63VDC',
-           '1K': '80VDC',
-           '2A': '100VDC',
-           '2C': '160VDC',
-           '2D': '200VDC',
-           '2E': '250VDC',
-           '2F': '315VDC',
-           '2V': '350VDC',
-           '2G': '400VDC',
-           '2W': '450VDC',
-           '2J': '630VDC',
-           '3A': '1000VDC',
+voltage = {'0E': '2.5V',
+           '0G': '4V',
+           '0J': '6.3V',
+           '1A': '10V',
+           '1C': '16V',
+           '1E': '25V',
+           '1V': '35V',
+           '1H': '50V',
+           '1J': '63V',
+           '1K': '80V',
+           '2A': '100V',
+           '2C': '160V',
+           '2D': '200V',
+           '2E': '250V',
+           '2F': '315V',
+           '2V': '350V',
+           '2G': '400V',
+           '2W': '450V',
+           '2J': '630V',
+           '3A': '1000V',
            'MF': '250VAC'}
 
 tolerance = {'B': Tolerance('-0.1pF', '+0.1pF'),
@@ -56,6 +56,7 @@ operating_temperature_range = {'VZ': lambda voltage : TemperatureRange('-55', '1
                                'CS': lambda voltage : TemperatureRange('-40', '105') if voltage < 401 else TemperatureRange('-25', '105'),
                                'CY': lambda voltage : TemperatureRange('-40', '105') if voltage < 401 else TemperatureRange('-25', '105')}
 
+packing = {'1TD': 'Unknown'}
 
 def build_regexpr(product_id):
     product_series_group = '(' + product_id + ')'  # 1
@@ -64,17 +65,19 @@ def build_regexpr(product_id):
     capacitance_group = '(R\d{2}|\dR\d|\d{3})'  # 4
     tolerance_group = build_group(tolerance)  # 5
     configuration_group = build_group(configuration)  # 6
-
-    return product_series_group + series_name_group + voltage_group + capacitance_group + tolerance_group + configuration_group + '?'
+    packing_group = build_group(packing)
+    return product_series_group + series_name_group + voltage_group + capacitance_group + tolerance_group +\
+           configuration_group  + packing_group + '?'
 
 
 def decode_match(match):
+    partnumber = match.group(1) + match.group(2) + match.group(3) + match.group(4) + match.group(5) + match.group(6)
+    partnumber += match.group(7) if match.group(7) is not None else ''
     voltage_str = voltage[match.group(3)]
     return Capacitor(capacitor_type=Capacitor.Type.ElectrolyticAluminium,
                      manufacturer="Nichicon",
-                     partnumber=match.group(1) + match.group(2) + match.group(3) + match.group(4) + match.group(
-                         5) + match.group(6),
-                     working_temperature_range=operating_temperature_range[match.group(2)](Decimal(voltage_str[:-3])),
+                     partnumber=partnumber,
+                     working_temperature_range=operating_temperature_range[match.group(2)](Decimal(voltage_str[:-1])),
                      series=match.group(2),
                      capacitance=capacitance_string_to_farads(match.group(4)) * Decimal('1000000'),
                      voltage=voltage_str,
