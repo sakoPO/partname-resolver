6abf09ba58a57ef4d22b44ff1964ab70b6d0350f
sakoPO <you@example.com>
2020-04-30 01:28:33 +0200

    Added chip case class

--- partname_resolver/case/chip.py
+++ partname_resolver/case/chip.py
@@ -1,26 +1,46 @@
+# Vishay H - height
+from partname_resolver.units.length import Dimmension, Length, LengthTolerance
+
+
 class Chip:
     class Dimensions:
-        iec_en_standard_dimensions = {'0402': {'L': 0.4, "W": 0.2},
-                                      '0404': {'L': 0.4, "W": 0.4},
-                                      '0603': {'L': 0.6, "W": 0.3},
-                                      '0505': {'L': 0.5, "W": 0.5},
-                                      '0805': {'L': 0.8, "W": 0.5}
+        iec_en_standard_dimensions = {'0402': {'L': Length('0.4mm'), "W": Length('0.2mm')},
+                                      '0404': {'L': Length('0.4mm'), "W": Length('0.4mm')},
+                                      '0603': {'L': Length('0.6mm'), "W": Length('0.3mm')},
+                                      '0505': {'L': Length('0.5mm'), "W": Length('0.5mm')},
+                                      '0805': {'L': Length('0.8mm'), "W": Length('0.5mm')},
+                                      '1005': {'L': Length('1.0mm'), "W": Length('0.5mm')},
+                                      '1608': {'L': Length('1.6mm'), "W": Length('1.8mm')},
+                                      '2520': {'L': Length('2.5mm'), "W": Length('2mm')},
+                                      '3216': {'L': Length('3.2mm'), "W": Length('1.6mm')}
                                       }
 
-        def __init__(self, L, W, T, t):
+        def __init__(self, L, W, T, t1, t2):
             self.L = L
             self.W = W
             self.T = T
-            self.t = t
+            self.t1 = t1
+            if t1 is not None and t2 is None:
+                self.t2 = t1
+            else:
+                self.t2 = t2
+
+        def __str__(self):
+            t1 = ", t1=" + str(self.t1) if self.t1 is not None else ""
+            t2 = ", t2=" + str(self.t2) if self.t2 is not None else ""
+            return "L=" + str(self.L) + ", W=" + str(self.W) + ", T=" + str(self.T) + t1 + t2
+
+        def __eq__(self, other):
+            return self.L == other.L and self.W == other.W and self.T == other.T and self.t1 == other.t1 and \
+                   self.t2 == other.t2
 
         @staticmethod
-        def from_IEC_EN_code(iec_en_code, T=0, t=0):
+        def from_IEC_EN_code(iec_en_code, T=0, t1=0, t2=0):
             tmp_dim = Chip.Dimensions.iec_en_standard_dimensions[iec_en_code]
-            return Chip.Dimensions(tmp_dim["L"], tmp_dim["W"], T, t)
+            return Chip.Dimensions(tmp_dim["L"], tmp_dim["W"], T, t1, t2)
 
-
-
-    eia_to_iec_en = {'1005': '0402',
+    eia_to_iec_en = {'01005': '0402',
+                     '1005': '0402',
                      '15015': '0404',
                      '0201': '0603',
                      '0202': '0505',
@@ -38,16 +58,26 @@ class Chip:
                      '1806': '4516'
                      }
 
-    def __init__(self, EIA_case_code, L=None, W=None, T=None, t=None):
+    def __init__(self, EIA_case_code, L=None, W=None, T=None, t1=None, t2=None):
         self.EIA_code = EIA_case_code
         self.IEC_EN_code = Chip.eia_to_iec_en[EIA_case_code]
-        if L is None and W is None and T is None and t is None:
-            self.dimensions = Chip.Dimensions.from_IEC_EN_code(self.IEC_EN_code)
+        if L is None and W is None:
+            self.dimensions = Chip.Dimensions.from_IEC_EN_code(self.IEC_EN_code, T=T, t1=t1, t2=t2)
         else:
-            self.dimensions = Chip.Dimensions(L, W, T, t)
+            self.dimensions = Chip.Dimensions(L, W, T, t1, t2)
+
+    def get_full_str(self):
+        return "EIA " + str(self.EIA_code) + ", IEC/EN " + self.IEC_EN_code + ", " + str(self.dimensions)
+
+    def __eq__(self, other):
+        return self.EIA_code == other.EIA_code and self.IEC_EN_code == other.IEC_EN_code and \
+               self.dimensions == other.dimensions
 
     def __str__(self):
-        return "EIA: " + str(self.EIA_code) + " IEC/EN:" + str(self.IEC_EN_code)
+        height = ", H=" + str(self.dimensions.T) if self.dimensions.T is not None else ''
+        return "EIA " + str(self.EIA_code) + height
 
     def __repr__(self):
-        return "Chip case, EIA: " + str(self.EIA_code) + ", IEC/EN: " + str(self.IEC_EN_code) + ", L=" + str(self.dimensions.L) + ",W=" + str(self.dimensions.W) + ",T=" + str(self.dimensions.T) + ", t=" + str(self.dimensions.t)
+        return "Chip case, EIA: " + str(self.EIA_code) + ", IEC/EN: " + str(self.IEC_EN_code) + ", L=" + str(
+            self.dimensions.L) + ",W=" + str(self.dimensions.W) + ",T=" + str(self.dimensions.T) + ", t=" + str(
+            self.dimensions.t)
---
+++ partname_resolver/case/test_chip.py
@@ -0,0 +1,22 @@
+import unittest
+from .chip import Chip
+from units.length import Dimmension, Length, LengthTolerance
+
+
+class ChipTestCase(unittest.TestCase):
+    def test_chip_name(self):
+        self.assertEqual("EIA 0402, H=0.35", str(Chip('0402', L=1, W=0.5, T=0.35, t1=0.25, t2=0.2)))
+        self.assertEqual("EIA 0402", str(Chip('0402')))
+        self.assertEqual("EIA 0402", Chip('0402', T=Dimmension('1mm', LengthTolerance('0.1mm'))).get_full_str())
+
+    def test_custom_dimmensions(self):
+        tolerance = LengthTolerance('0.1mm')
+        chip = Chip('0603', L=Dimmension('1.6mm', tolerance), W=Dimmension('0.8mm', tolerance),
+                    T=Dimmension('0.45mm', tolerance), t1=Dimmension('0.3mm', LengthTolerance('0.2mm')),
+                    t2=Dimmension('0.3mm', LengthTolerance('0.2mm')))
+        self.assertEqual("EIA 0603, IEC/EN 1608, L=1.6±0.1mm, W=0.8±0.1mm, T=0.45±0.1mm, t1=0.3±0.2mm, t2=0.3±0.2mm", chip.get_full_str())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
